ba96ed13f6698b846bd0e77f78af47a6bb894d06
TarcisioJr-dev <tarcisioalgoritmo@gmail.com>
2026-03-25 06:58:51 -0300

    atualização no código principal.

--- exemplo_listas/src/principal.java
+++ exemplo_listas/src/principal.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 
+// exemplo de TRY/CATCH utilizado:
+// try {
+    
+// } catch (NumberFormatException e) {
+//     System.out.println("[ERRO] Você digitou um caractere inválido.");
+//     continue;
+// }
+
 public class principal {
 
     public static void main(String[] args) {
@@ -15,8 +23,9 @@ public class principal {
             System.out.println(" 3. Lista encadeada");
             System.out.println(" 4. DEQUE");
             System.out.println(" 5. Sair");
-
+            System.out.print("\nDigite sua opção: ");
             String entrada = leitor.nextLine();
+
             try {
                 int opcao = Integer.parseInt(entrada); // Tenta converter o texto em número
                 
@@ -29,9 +38,40 @@ public class principal {
                     switch (opcao) {
                         case 1: // Opções de Pilha
                         System.out.println("\n--- Opções de Pilha ---");
-                        System.out.println(" 1. Adicionar item");
-                        System.out.println(" 2. Mostrar pilha");
-                        System.out.println(" 3. Remover item");
+                        System.out.println(" 1. Criar Pilha");
+                        System.out.println(" 2. Adicionar item");
+                        System.out.println(" 3. Mostrar pilha");
+                        System.out.println(" 4. Remover item");
+                        System.out.print("\n Digite sua opção: ");
+                        entrada = leitor.nextLine();
+
+                        try {
+                            int opcaoPilha = Integer.parseInt(entrada);
+
+                            switch (opcaoPilha) {
+                                case 1:
+                                    System.out.println("Digite o tamanho da PILHA que deseja:");
+                                    System.out.print("--> ");
+                                    entrada = leitor.nextLine();
+                                    try {
+                                        int tamanhoPilha = Integer.parseInt(entrada);
+                                        Pilha minhaPilha = new Pilha(tamanhoPilha);
+                                        System.out.println("Sua pilha foi criada com %d espaços", tamanhoPilha);
+                                        
+                                    } catch (NumberFormatException e) {
+                                        System.out.println("[ERRO] Você digitou um caractere inválido.");
+                                        continue;
+                                    }
+                                    
+                                    break;
+                            
+                                default:
+                                    break;
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("[ERRO] Você digitou um caractere inválido.");
+                            continue;
+                        }
                         break;
                         
                         case 5: // Sair
