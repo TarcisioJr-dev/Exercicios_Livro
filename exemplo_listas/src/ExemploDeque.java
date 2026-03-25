6eb750c75fe552740059307d0c7f3d6c4fafd96f
TarcisioJr-dev <tarcisioalgoritmo@gmail.com>
2026-03-24 22:30:34 -0300

    Criação de código e da lógica

--- exemplo_listas/src/ExemploDeque.java
+++ exemplo_listas/src/ExemploDeque.java
@@ -5,22 +5,12 @@ public class ExemploDeque {
         //Usando a implementação nativa para simplificar o conceito
         LinkedList<Integer> deque = new LinkedList<>();
 
-        deque.addFirst(10); // Inserir no início
-        deque.addLast(20); // Inserir no fim
+        deque.add(10); // Inserir no início
 
         System.out.println(deque);
         
         int primeiro = deque.removeFirst(); // Remove do início (10)
         int ultimo = deque.removeLast(); // Remove do fim (20)
-        
-        System.out.println(deque);
-        
-        System.out.println(primeiro);
-        System.out.println(ultimo);
-        
-        deque.add(primeiro);
-        deque.add(ultimo);
 
-        System.out.println(deque);
     }
 }
--- exemplo_listas/src/principal.java
+++ exemplo_listas/src/principal.java
@@ -1,6 +1,53 @@
+import java.util.Scanner;
+
 public class principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner leitor = new Scanner(System.in);
+
+        boolean loop = true;
+
+        while (loop) {
+            System.out.println("\n--- Menu de opções ---");
+            System.out.println(" 1. Pilha");
+            System.out.println(" 2. Fila");
+            System.out.println(" 3. Lista encadeada");
+            System.out.println(" 4. DEQUE");
+            System.out.println(" 5. Sair");
+
+            String entrada = leitor.nextLine();
+            try {
+                int opcao = Integer.parseInt(entrada); // Tenta converter o texto em número
+                
+                if (opcao < 1 || opcao > 5) {
+                    
+                    System.out.println("[ERRO] Opção Inválida, escolha novamente.");
+                    continue;
+                    
+                } else {
+                    switch (opcao) {
+                        case 1: // Opções de Pilha
+                        System.out.println("\n--- Opções de Pilha ---");
+                        System.out.println(" 1. Adicionar item");
+                        System.out.println(" 2. Mostrar pilha");
+                        System.out.println(" 3. Remover item");
+                        break;
+                        
+                        case 5: // Sair
+                        System.out.println("Obrigado por utilizar este programa.");
+                        loop = false;
+                        
+                        default:
+                            break;
+                        }
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("[ERRO] Você digitou um caractere inválido.");
+                continue;
+            }
+        }
+
+        leitor.close();
     }
 }
